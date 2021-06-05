6833a44036376f1a19a1c9deef2782bd0b7e7882
Björn Ricks <bjoern.ricks@greenbone.net>
2021-06-05 17:40:23 +0200

    Change styling of the docs
    Use the styling from python-gvm for now.

--- conf.py
+++ conf.py
@@ -28,6 +28,8 @@ author = 'Greenbone Networks'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -36,7 +38,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -46,7 +48,25 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'alabaster'
 
+html_favicon = 'favicon.gif'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+    'github_user': 'greenbone',
+    'github_repo': 'docs',
+    'github_banner': True,
+    'show_powered_by': False,
+}
+
+html_sidebars = {
+    '**': [
+        'sidebarlogo.html',
+        'localtoc.html',
+        'relations.html',
+        'searchbox.html',
+    ]
+}
