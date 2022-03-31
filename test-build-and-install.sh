4ba12ad979327c39ebf01a3102147fb74e516e3a
Björn Ricks <bjoern.ricks@greenbone.net>
2022-03-31 14:44:48 +0200

    Add scripts for testing the docs
    Add bash scripts for trying out most of the commands from the docs. The
    files need to be updated allong with the docs. They can be run within a
    docker container for quick testing
    
    `docker run -it --rm --name test-docs debian:stable-slim /bin/bash`
    `docker cp test-build-and-install.sh test-docs:/tmp`
    `docker cp test-env.sh test-docs:/tmp`
    `docker cp test-setup.sh test-docs:/tmp`

---
+++ test-build-and-install.sh
@@ -0,0 +1,256 @@
+#!/bin/bash
+
+set -e
+
+source ./test-env.sh
+
+mkdir -p $SOURCE_DIR
+mkdir -p $BUILD_DIR
+mkdir -p $INSTALL_DIR
+
+$APT_UPDATE
+$APT_INSTALL \
+  build-essential \
+  curl \
+  cmake \
+  pkg-config \
+  python3 \
+  python3-pip \
+  gnupg
+
+# gvm-libs
+
+$APT_INSTALL \
+ libglib2.0-dev \
+  libgpgme-dev \
+  libgnutls28-dev \
+  uuid-dev \
+  libssh-gcrypt-dev \
+  libhiredis-dev \
+  libxml2-dev \
+  libpcap-dev \
+  libnet1-dev \
+  libldap2-dev \
+  libradcli-dev
+
+curl -f -L https://github.com/greenbone/gvm-libs/archive/refs/tags/v$GVM_LIBS_VERSION.tar.gz -o $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION.tar.gz
+
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION.tar.gz
+
+mkdir -p $BUILD_DIR/gvm-libs && cd $BUILD_DIR/gvm-libs
+
+cmake $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION \
+  -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DSYSCONFDIR=/etc \
+  -DLOCALSTATEDIR=/var
+
+make -j$(nproc)
+
+make DESTDIR=$INSTALL_DIR install
+
+$SUDO cp -rv $INSTALL_DIR/* /
+
+rm -rf $INSTALL_DIR/*
+
+# gvmd
+
+$APT_INSTALL \
+  libglib2.0-dev \
+  libgnutls28-dev \
+  libpq-dev \
+  postgresql-server-dev-13 \
+  libical-dev \
+  xsltproc \
+  rsync \
+  texlive-latex-extra \
+  texlive-fonts-recommended \
+  xmlstarlet \
+  zip \
+  rpm \
+  fakeroot \
+  dpkg \
+  nsis \
+  gnupg \
+  gpgsm \
+  wget \
+  sshpass \
+  openssh-client \
+  socat \
+  snmp \
+  python3 \
+  smbclient \
+  python3-lxml \
+  gnutls-bin \
+  xml-twig-tools
+
+curl -f -L https://github.com/greenbone/gvmd/archive/refs/tags/v$GVMD_VERSION.tar.gz -o $SOURCE_DIR/gvmd-$GVMD_VERSION.tar.gz
+
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/gvmd-$GVMD_VERSION.tar.gz
+
+mkdir -p $BUILD_DIR/gvmd && cd $BUILD_DIR/gvmd
+
+cmake $SOURCE_DIR/gvmd-$GVMD_VERSION \
+  -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLOCALSTATEDIR=/var \
+  -DSYSCONFDIR=/etc \
+  -DGVM_DATA_DIR=/var \
+  -DGVMD_RUN_DIR=/run/gvmd \
+  -DOPENVAS_DEFAULT_SOCKET=/run/ospd/ospd-openvas.sock \
+  -DGVM_FEED_LOCK_PATH=/var/lib/gvm/feed-update.lock \
+  -DSYSTEMD_SERVICE_DIR=/lib/systemd/system \
+  -DDEFAULT_CONFIG_DIR=/etc/default \
+  -DLOGROTATE_DIR=/etc/logrotate.d
+
+make -j$(nproc)
+
+make DESTDIR=$INSTALL_DIR install
+
+$SUDO cp -rv $INSTALL_DIR/* /
+
+rm -rf $INSTALL_DIR/*
+
+# gsa
+
+$APT_INSTALL \
+  nodejs \
+  yarnpkg
+
+curl -f -L https://github.com/greenbone/gsa/archive/refs/tags/v$GSA_VERSION.tar.gz -o $SOURCE_DIR/gsa-$GSA_VERSION.tar.gz
+
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/gsa-$GSA_VERSION.tar.gz
+
+cd $SOURCE_DIR/gsa-$GSA_VERSION
+
+rm -rf build
+
+yarnpkg
+yarnpkg build
+
+$SUDO mkdir -p $INSTALL_PREFIX/share/gvm/gsad/web/
+$SUDO cp -r build/* $INSTALL_PREFIX/share/gvm/gsad/web/
+
+# gsad
+
+$APT_INSTALL \
+  libmicrohttpd-dev \
+  libxml2-dev \
+  libglib2.0-dev \
+  libgnutls28-dev
+
+curl -f -L https://github.com/greenbone/gsad/archive/refs/tags/v$GSAD_VERSION.tar.gz -o $SOURCE_DIR/gsad-$GSAD_VERSION.tar.gz
+
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/gsad-$GSAD_VERSION.tar.gz
+
+mkdir -p $BUILD_DIR/gsad && cd $BUILD_DIR/gsad
+
+cmake $SOURCE_DIR/gsad-$GSAD_VERSION \
+  -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DSYSCONFDIR=/etc \
+  -DLOCALSTATEDIR=/var \
+  -DGVMD_RUN_DIR=/run/gvmd \
+  -DGSAD_RUN_DIR=/run/gsad \
+  -DLOGROTATE_DIR=/etc/logrotate.d
+
+make -j$(nproc)
+
+make DESTDIR=$INSTALL_DIR install
+
+$SUDO cp -rv $INSTALL_DIR/* /
+
+rm -rf $INSTALL_DIR/*
+
+# openvas-smb
+
+$APT_INSTALL \
+  gcc-mingw-w64 \
+  libgnutls28-dev \
+  libglib2.0-dev \
+  libpopt-dev \
+  libunistring-dev \
+  heimdal-dev \
+  perl-base
+
+curl -f -L https://github.com/greenbone/openvas-smb/archive/refs/tags/v$OPENVAS_SMB_VERSION.tar.gz -o $SOURCE_DIR/openvas-smb-$OPENVAS_SMB_VERSION.tar.gz
+
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/openvas-smb-$OPENVAS_SMB_VERSION.tar.gz
+
+mkdir -p $BUILD_DIR/openvas-smb && cd $BUILD_DIR/openvas-smb
+
+cmake $SOURCE_DIR/openvas-smb-$OPENVAS_SMB_VERSION \
+  -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+  -DCMAKE_BUILD_TYPE=Release
+
+make -j$(nproc)
+
+make DESTDIR=$INSTALL_DIR install
+
+$SUDO cp -rv $INSTALL_DIR/* /
+
+rm -rf $INSTALL_DIR/*
+
+# openvas-scanner
+
+$APT_INSTALL \
+  bison \
+  libglib2.0-dev \
+  libgnutls28-dev \
+  libgcrypt20-dev \
+  libpcap-dev \
+  libgpgme-dev \
+  libksba-dev \
+  rsync \
+  nmap \
+  python3-impacket \
+  libsnmp-dev
+
+curl -f -L https://github.com/greenbone/openvas-scanner/archive/refs/tags/v$OPENVAS_SCANNER_VERSION.tar.gz -o $SOURCE_DIR/openvas-scanner-$OPENVAS_SCANNER_VERSION.tar.gz
+
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/openvas-scanner-$OPENVAS_SCANNER_VERSION.tar.gz
+
+mkdir -p $BUILD_DIR/openvas-scanner && cd $BUILD_DIR/openvas-scanner
+
+cmake $SOURCE_DIR/openvas-scanner-$OPENVAS_SCANNER_VERSION \
+  -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DSYSCONFDIR=/etc \
+  -DLOCALSTATEDIR=/var \
+  -DOPENVAS_FEED_LOCK_PATH=/var/lib/openvas/feed-update.lock \
+  -DOPENVAS_RUN_DIR=/run/ospd
+
+make -j$(nproc)
+
+make DESTDIR=$INSTALL_DIR install
+
+$SUDO cp -rv $INSTALL_DIR/* /
+
+rm -rf $INSTALL_DIR/*
+
+# ospd-openvas
+
+$APT_INSTALL \
+  python3 \
+  python3-pip \
+  python3-setuptools \
+  python3-packaging \
+  python3-wrapt \
+  python3-cffi \
+  python3-psutil \
+  python3-lxml \
+  python3-defusedxml \
+  python3-paramiko \
+  python3-redis
+
+curl -f -L https://github.com/greenbone/ospd-openvas/archive/refs/tags/v$OSPD_OPENVAS_VERSION.tar.gz -o $SOURCE_DIR/ospd-openvas-$OSPD_OPENVAS_VERSION.tar.gz
+
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/ospd-openvas-$OSPD_OPENVAS_VERSION.tar.gz
+
+cd $SOURCE_DIR/ospd-openvas-$OSPD_OPENVAS_VERSION
+
+python3 -m pip install . --prefix=$INSTALL_PREFIX --root=$INSTALL_DIR --no-warn-script-location
+
+$SUDO cp -rv $INSTALL_DIR/* /
+
+rm -rf $INSTALL_DIR/*
