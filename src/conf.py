f3b1c11ec8389fe960c491d822b24af0d7d7d71f
Björn Ricks <bjoern.ricks@greenbone.net>
2022-08-04 09:30:56 +0200

    Change: Use furo theme for our docs
    Furo has a nicer appearance for the table of contents and looks more
    "clean" overall.

--- src/conf.py
+++ src/conf.py
@@ -53,7 +53,8 @@ source_suffix = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "furo"
+html_title = project
 
 html_favicon = "favicon.png"
 
@@ -61,21 +62,26 @@ html_favicon = "favicon.png"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["custom.css"]
+html_logo = "_static/logo.svg"
 
+repo_url = "https://github.com/greenbone/docs/"
 html_theme_options = {
-    "github_user": "greenbone",
-    "github_repo": "docs",
-    "github_banner": True,
-    "show_powered_by": False,
-}
-
-html_sidebars = {
-    "**": [
-        "sidebarlogo.html",
-        "localtoc.html",
-        "relations.html",
-        "searchbox.html",
-    ]
+    "source_repository": repo_url,
+    "source_branch": "main",
+    "source_directory": "src/",
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": repo_url,
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
 }
 
 # https://myst-parser--467.org.readthedocs.build/en/467/syntax/optional.html#auto-generated-header-anchors
